ae0beecc4fa70ea17b5730fe4103dbe10c53fb45
devgianlu <altomanigianluca@gmail.com>
2025-04-08 19:08:50 +0200

    Export everything that is needed

--- lib/upnp_client.dart
+++ lib/upnp_client.dart
@@ -1,3 +1,11 @@
 library;
 
 export 'src/discovery.dart';
+export 'src/device.dart';
+export 'src/service.dart';
+export 'src/data_type.dart';
+export 'src/action.dart';
+
+export 'src/common_services/av_transport.dart';
+export 'src/common_services/connection_manager.dart';
+export 'src/common_services/rendering_control.dart';
